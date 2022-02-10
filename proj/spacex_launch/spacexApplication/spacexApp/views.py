b9a33f6b4c338c988e32916dea9c329940a55d8f
Robert <reyesrober95@gmail.com>
2022-02-09 19:43:28 -0800

    fxing changes to on view

--- proj/spacex_launch/spacexApplication/spacexApp/views.py
+++ proj/spacex_launch/spacexApplication/spacexApp/views.py
@@ -6,12 +6,3 @@ import requests
 
 def index(request):
     return render(request, 'spacexApp/index.html')
-
-"""
-# We may not need this part
-def upcoming_launch(request):
-    url = "https://api.spacexdata.com/v3/launches/upcoming"
-    response = requests.get(url)
-    data = response.json()
-    return render(request, "spacexApp/index.html", {"data": data})
-"""
