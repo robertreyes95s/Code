cc9caa1cf6b4dc9854684ef9b13f6e3fd3a67903
Robert <reyesrober95@gmail.com>
2022-06-04 21:07:09 -0700

    Adding two practice mini projects

--- practice/practice-problems/password_gen.py
+++ practice/practice-problems/password_gen.py
@@ -1,34 +1,46 @@
 # this is a random password generator
 import random 
+import pyinputplus as pyip
+
 
 def pass_random(): 
+
+    # All characters that make passwords are generated from 
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     alpha_cap = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    special_car = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '{', '}', '[', ']', '<', '>', '?']
 
-    all_characters = numbers + alpha + alpha_cap
-    print(all_characters)
-    choice = input("Length of password? - no longer than 15 charachters")
-    if choice <= 15: 
-        pass
-    else: 
-        exit()
+    #All characters combined in one pool 
+    all_characters = numbers + alpha + alpha_cap + special_car
 
-    password = 
+    # Next up sort it so you can have choices wether to haev upercase or no uppercase, no special character, or only lettter no numbers 
+    # and all those combintions of them
 
-    
+    #Final Password string
+    password = ""
+
+    #Length of password determinded by the user 
+    choice = input("Length of password? - no longer than 15 charachters\n")
+
+    #Generator of passwords
+    if int(choice) <= 15:
+        for i in range(int(choice)):
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)        
+    else:
+        print("Count to high")
 
 
 print("Random Password Generator\n")
 
-action = input("Generate password?\n")
-if action == 'y': 
+action = pyip.inputYesNo("Generate password? (yes or no) \n")
+if action == 'yes': 
     pass
-elif action == 'n': 
+elif action == 'no': 
     exit()
-else:
-    pass
-
+else: 
+    exit()
 pass_random()
-
-
--- practice/practice-problems/rock_paper_scic.py
+++ practice/practice-problems/rock_paper_scic.py
@@ -1,67 +1,91 @@
 # Python Pratice Problem - Rock Paper Scissors 
 # this is a two player game
 # python3
-import time
 import random
 
 def game():
-    player1_name = input("Enter a name for Player 1: ")
-    player1 = player1_name
-
+    player1_name = input("Enter a name: ")
+    
     turns = 0
     player1_score = 0
     ai_score = 0
+    choices_ai = [1, 2, 3]
 
     while turns < 3:
 
-        choices_ai = [1, 2, 3]
-
-        p1_choice = input(player1_name + " choose: Rock[1], paper[2], or scissors[3]\n")
+        choice = input(player1_name + " choose: Rock[1], paper[2], or scissors[3]\n")
         ai_choice = random.choice(choices_ai)
+
+        p1_choice = int(choice)
 
         if p1_choice == 1 and ai_choice == 1:
             turns += 1
             print("Draw")
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score)+ "\n")
+            print("ai chose Rock")
         elif p1_choice == 1 and ai_choice == 2:
             print("Point to Ai Bob")
             ai_score += 1
-            turn += 1 
+            turns += 1 
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score)+ "\n") 
+            print("ai chose Paper")                  
         elif p1_choice == 1 and ai_choice == 3:
             print("Point to " + player1_name)
             player1_score += 1 
-            turn += 1
+            turns += 1
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score)+ "\n") 
+            print("ai chose Scissors")       
         elif p1_choice == 2 and ai_choice == 1:
             print("Point to " + player1_name)
             player1_score += 1 
-            turn += 1 
+            turns += 1
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score))           
         elif p1_choice == 2 and ai_choice == 2: 
             turns  += 1 
             print("Draw")
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score))        
         elif p1_choice == 2 and ai_choice == 3: 
             print("Paint to Ai bob")
             ai_score += 1
-            turn += 1 
+            turns += 1 
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score))
         elif p1_choice == 3 and ai_choice == 1: 
             print("Point to Ai Bob")
             ai_score += 1
-            turn += 1
+            turns += 1
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score))
         elif p1_choice == 3 and ai_choice == 2: 
-            print('Point to' + player1_name)
+            print('Point to ' + player1_name)
             player1_score += 1
             turns += 1
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score))
         elif p1_choice == 3 and ai_choice == 3:
             print("Draw")
             turns += 1 
+            print("p1 score: " + str(player1_score))
+            print("ai score: " + str(ai_score))
+        else: 
+            print("Enter Valid Response")
         
     else: 
-        print("Game Over")
+        print("Game Over\n")
+        print("p1 score: " + str(player1_score))
+        print("ai score: " + str(ai_score))
     
-    if player1_score < ai_choice: 
+    if player1_score < ai_score: 
         print("Ai Bob Wins")
     else:
         print(player1_name + " Wins")
-
-print("Hello, welcome to Rock Paper Scissors")
+print()
+print("Hello, Welcome to Rock Paper Scissors\n")
 determine_game = input("Do you want to play?\n")
 if determine_game == "Yes" or 'y' or "Y" or "YES" or "yes":
     game()
