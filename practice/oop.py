0af6bd9e696fda8480c5f6b592ea42255f77d6de
Robert <reyesrober95@gmail.com>
2022-06-10 16:25:41 -0700

    Adding password_gen changes

---
+++ practice/oop.py
@@ -0,0 +1,5 @@
+import os
+
+print("This is just a test")
+value = 123
+print(value)
--- practice/practice-problems/password_gen.py
+++ practice/practice-problems/password_gen.py
@@ -1,5 +1,5 @@
 # this is a random password generator
-import random 
+import random
 import pyinputplus as pyip
 
 
@@ -12,7 +12,7 @@ def pass_random():
     special_car = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '{', '}', '[', ']', '<', '>', '?']
 
     #All characters combined in one pool 
-    all_characters = numbers + alpha + alpha_cap + special_car
+    #all_characters = numbers + alpha +  alpha_cap +  special_car
 
     # Next up sort it so you can have choices wether to haev upercase or no uppercase, no special character, or only lettter no numbers 
     # and all those combintions of them
@@ -21,19 +21,119 @@ def pass_random():
     password = ""
 
     #Length of password determinded by the user 
-    choice = input("Length of password? - no longer than 15 charachters\n")
+    length_choice = pyip.inputInt("Length of password? - no longer than 15 charachters\n", min=1, max=17)
+    alph_cap_choice = pyip.inputYesNo("Include captilized alpha characters?\n")
+    alpha_choice = pyip.inputYesNo("Inlcude lower case alpha characters?\n")
+    special_char_choice = pyip.inputYesNo('Include special characters?\n')
+    num_choice = pyip.inputYesNo('Include numbers?\n')
+
+
 
     #Generator of passwords
-    if int(choice) <= 15:
-        for i in range(int(choice)):
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "yes" and special_char_choice == "yes" and num_choice == "yes":
+        all_characters = numbers + alpha + alpha_cap + special_car
+        for i in range(int(length_choice)):
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else:
+            print(password) 
+
+    if int(length_choice) <= 15 and alph_cap_choice == "no" and alpha_choice == "yes" and special_char_choice == "yes" and num_choice == "yes":
+        all_characters = numbers + alpha + special_car
+        for i in range(int(length_choice)):
             cho = random.choice(all_characters)
             password += str(cho)
         else: 
-            print(password)        
-    else:
-        print("Count to high")
+            print(password)
 
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "no" and special_char_choice == "yes" and num_choice == "yes":
+        all_characters = numbers + alpha_cap + special_car
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
 
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "yes" and special_char_choice == "no" and num_choice == "yes":
+        all_characters = numbers + alpha + alpha_cap
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "yes" and special_char_choice == "yes" and num_choice == "no":
+        all_characters = alpha + alpha_cap + special_car
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "no" and alpha_choice == "no" and special_char_choice == "yes" and num_choice == "yes":
+        all_characters = special_car + numbers
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "yes" and special_char_choice == "no" and num_choice == "no":
+        all_characters = alpha_cap + alpha
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "no" and alpha_choice == "no" and special_char_choice == "no" and num_choice == "yes":
+        all_characters = numbers
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "no" and special_char_choice == "no" and num_choice == "no":
+        all_characters = alpha_cap
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "no" and alpha_choice == "yes" and special_char_choice == "yes" and num_choice == "no":
+        all_characters = alpha + special_car
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "no" and special_char_choice == "no" and num_choice == "yes":
+        all_characters = alpha_cap + numbers
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "yes" and alpha_choice == "no" and special_char_choice == "yes" and num_choice == "no":
+        all_characters = alpha_cap + special_car
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+
+    if int(length_choice) <= 15 and alph_cap_choice == "no" and alpha_choice == "yes" and special_char_choice == "no" and num_choice == "yes":
+        all_characters = alpha + numbers
+        for i in range(int(length_choice)): 
+            cho = random.choice(all_characters)
+            password += str(cho)
+        else: 
+            print(password)
+    
 print("Random Password Generator\n")
 
 action = pyip.inputYesNo("Generate password? (yes or no) \n")
